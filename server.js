be518ca86626c6f9e07b12cc272a74c01bedb2fa
Krantisinh Deshmukh <krantisinh07@gmail.com>
2019-07-16 10:17:33 +0530

    Created a sample hapi server to serve an html file.

---
+++ server.js
@@ -0,0 +1,33 @@
+'use strict';
+const Hapi = require('@hapi/hapi');
+const Inert = require('@hapi/inert');
+
+const init = async () => {
+
+    const server = Hapi.server({
+        port: 3000,
+        host: 'localhost'
+    });
+
+    await server.register(Inert);
+
+    server.route({
+        method: 'GET',
+        path:'/',
+        handler: (request, h) => {
+
+            return h.file('./src/index.html');
+        }
+    });
+
+    await server.start();
+    console.log('Server running on %s', server.info.uri);
+};
+
+process.on('unhandledRejection', (err) => {
+
+    console.log(err);
+    process.exit(1);
+});
+
+init();
